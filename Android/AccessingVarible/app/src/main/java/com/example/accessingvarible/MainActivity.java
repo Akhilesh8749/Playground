7922d94a36f0d9de131a63d5da484bbdadf1e54d
Akhilesh M M <malaganvar1998@gmail.com>
2019-09-09 22:37:37 +0530

    Android
     Toast message,Button Click,App permissions,

--- Android/AccessingVarible/app/src/main/java/com/example/accessingvarible/MainActivity.java
+++ Android/AccessingVarible/app/src/main/java/com/example/accessingvarible/MainActivity.java
@@ -3,15 +3,24 @@ package com.example.accessingvarible;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+ Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        emp e=new emp();
+        btn=findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                emp e=new emp();
+                e.myFun(getApplicationContext());
+            }
+        });
 
     }
 }
--- Android/AccessingVarible/app/src/main/java/com/example/accessingvarible/emp.java
+++ Android/AccessingVarible/app/src/main/java/com/example/accessingvarible/emp.java
@@ -1,8 +1,27 @@
 package com.example.accessingvarible;
 
 
-public class emp extends MainActivity{
-    public emp(){
-        System.out.println("Akhilesh M Malaganavar");
+import android.content.Context;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class emp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
     }
+
+
+    emp() {
+            System.out.println("Akhilesh M Malaganavar");
+    }
+
+
+ public void myFun(Context applicationContext){
+     Toast.makeText(applicationContext, "Akhilesh", Toast.LENGTH_SHORT).show();
+ }
 }
