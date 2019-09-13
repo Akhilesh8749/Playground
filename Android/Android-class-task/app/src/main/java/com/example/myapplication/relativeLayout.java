58532923aecbe357fa5ad379f79b5a9cb63bd4d0
Akhilesh M M <malaganvar1998@gmail.com>
2019-09-14 00:57:17 +0530

    Android
    Frame Layout

--- Android/Android-class-task/app/src/main/java/com/example/myapplication/relativeLayout.java
+++ Android/Android-class-task/app/src/main/java/com/example/myapplication/relativeLayout.java
@@ -2,13 +2,25 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class relativeLayout extends AppCompatActivity {
-
+ Button btn2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_relative_layout);
+    btn2=findViewById(R.id.button2);
+
+    btn2.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Intent frame_layout= new Intent(getApplicationContext(),FrameLayout.class);
+            startActivity(frame_layout);
+        }
+    });
     }
 }
