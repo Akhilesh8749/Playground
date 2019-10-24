9e763466ab65fffbdea851925fd5b824c40367e5
Akhilesh M M <malaganvar1998@gmail.com>
2019-10-24 11:21:57 +0530

    POD Code

--- Team Project/PODS-Drag Drop/styleJS.js
+++ Team Project/PODS-Drag Drop/styleJS.js
@@ -2,25 +2,31 @@ $(document).ready(function() {
     var d_canvas = document.getElementById('canvas');
     var context = d_canvas.getContext('2d');
     var background = document.getElementById('background');
-    var pod = document.getElementById('pod');
-    var reset = document.getElementById('reset')
-    context.drawImage(background, 0, 0);
-    
+    var pod = document.getElementById('podID');
 
-    $('#pod').draggable();
+    context.drawImage(background, 0, 0);
+    $('#valueX').text(00);
+    $('#valueY').text(00);
+    $("#draw").attr("disabled", true);
+
+    // $('#addPOD').click(function(){
+    //     context.drawImage(pod,200,200);
+    // });
+
+    $('#podID').draggable();
 
     $('#draw').click(function() {
-        var $pod = $('#pod');
+        var $pod = $('#podID');
         var $canvas = $('#canvas');
-        var $reset = $('#reset')
         var pod_x = $pod.offset().left - $canvas.offset().left;
         var pod_y = $pod.offset().top - $canvas.offset().top;
 
         context.drawImage(pod, pod_x, pod_y);
+        $('#valueX').text(pod_x);
+        $('#valueY').text(pod_y);
         
         $pod.hide();
         $(this).attr('disabled', 'disabled');
-
-        $reset.reset();
     });
+
 });
